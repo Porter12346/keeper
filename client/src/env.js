264ba8f0a80212d2d0f57809823a7846986bb978
Porter Young <porteryoung2005@gmail.com>
2024-09-04 16:50:17 -0600

    deploy

--- client/src/env.js
+++ client/src/env.js
@@ -1,7 +1,7 @@
 export const dev = window.location.origin.includes('localhost')
 
 // REVIEW don't forget to change this URL if you're using a dotnet backend
-export const baseURL = dev ? 'https://localhost:7045' : ''
+export const baseURL = dev ? 'https://localhost:7045' : 'https://keeper.porteryoung.dev'
 export const useSockets = false
 export const domain = 'dev-hhcu2170fadrfsij.us.auth0.com'
 export const clientId = '9ky5h2ESpCSBmkWByEuMajyVhAiNBsZN'
