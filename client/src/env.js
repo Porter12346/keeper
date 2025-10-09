a67c99b88a8addc1b540b9ce07245efd03dafd92
Porter Young <porteryoung2005@gmail.com>
2025-10-08 22:08:51 -0600

    bang

--- client/src/env.js
+++ client/src/env.js
@@ -1,7 +1,7 @@
 export const dev = window.location.origin.includes('localhost')
 
 // REVIEW don't forget to change this URL if you're using a dotnet backend
-export const baseURL = dev ? 'https://localhost:7045' : 'https://keeper.porteryoung.dev'
+export const baseURL = dev ? 'https://localhost:7045' : 'https://keeperporter-erevc0czdrfxdffa.canadacentral-01.azurewebsites.net'
 export const useSockets = false
 export const domain = 'dev-hhcu2170fadrfsij.us.auth0.com'
 export const clientId = '9ky5h2ESpCSBmkWByEuMajyVhAiNBsZN'
